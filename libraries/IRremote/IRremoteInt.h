fa9a7ea8822674560201918563d6869fe16873e0
Paul Clarke <pauly@clarkeology.com>
2012-05-24 19:43:18 +0100

    lib name changed

--- libraries/IRremote/IRremoteInt.h
+++ libraries/IRremote/IRremoteInt.h
@@ -12,7 +12,7 @@
 #ifndef IRremoteint_h
 #define IRremoteint_h
 
-#include <WProgram.h>
+#include <Arduino.h>
 
 #define CLKFUDGE 5      // fudge factor for clock interrupt overhead
 #define CLK 256      // max value for clock (timer 2)
